d08308775bd31fba8ea06d7f4f6de774f985c9d1
shengrong <shengrong_88@hotmail.com>
2021-01-14 16:11:15 +0800

    1. added tab nav 2. added redux

--- App.js
+++ App.js
@@ -3,21 +3,33 @@ import React, { useState, useEffect } from "react";
 import { ActivityIndicator, StyleSheet, Text, View } from "react-native";
 import SignInScreen from "./screens/SignInScreen";
 import SignUpScreen from "./screens/SignUpScreen";
-import AccountScreen from "./screens/AccountScreen";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import TabStack from "./components/TabStack"
+import { useSelector, useDispatch, Provider } from "react-redux";
+import { signInAction } from "./redux/ducks/blogAuth";
+import store from "./redux/createStore";
 
 const Stack = createStackNavigator();
 
-export default function App() {
+export default function AppWrapper() {
+  return (
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+}
+
+function App() {
   const [loading, setLoading] = useState(true);
-  const [signedIn, setSignedIn] = useState(false);
+  const dispatch = useDispatch();
+  const signedIn = useSelector((state) => state.auth.signedIn);
 
   async function loadToken() {
     const token = await AsyncStorage.getItem("token");
     if (token) {
-      setSignedIn(true);
+      dispatch(signInAction());
     }
     setLoading(false);
   }
@@ -26,17 +38,28 @@ export default function App() {
     loadToken();
   }, []);
 
-  return loading ? (
-    <View style={styles.container}>
-      <ActivityIndicator />
-    </View>
-  ) : (
+  if (loading) {
+    return (
+      <View style={styles.container}>
+        <ActivityIndicator />
+      </View>
+    );
+  }
+
+  return (
     <NavigationContainer>
-      <Stack.Navigator mode="modal" headerMode="none" initialRouteName={signedIn ? "Account" : "SignIn"}>
-        <Stack.Screen component={AccountScreen} name="Account" />
-        <Stack.Screen component={SignInScreen} name="SignIn" />
-        <Stack.Screen component={SignUpScreen} name="SignUp" />
-      </Stack.Navigator>
+            {signedIn ? (
+        <TabStack />
+      ) : (
+        <Stack.Navigator
+          mode="modal"
+          headerMode="none"
+          screenOptions={{ animationEnabled: false }}
+        >
+          <Stack.Screen component={SignInScreen} name="SignIn" />
+          <Stack.Screen component={SignUpScreen} name="SignUp" />
+        </Stack.Navigator>
+      )}
     </NavigationContainer>
   );
 }
--- screens/AccountScreen.js
+++ screens/AccountScreen.js
@@ -3,12 +3,16 @@ import { Button, StyleSheet, Text, View, ActivityIndicator } from "react-native"
 import { commonStyles } from "../styles/commonStyles";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useUsername } from "../hooks/useAPI";
+import { useDispatch } from "react-redux";
+import { signOutAction } from "../redux/ducks/blogAuth";
 
 export default function AccountScreen({ navigation }) {
   const [username, loading, error, refresh] = useUsername();
+  const dispatch = useDispatch();
 
   // signs out if the useUsername hook returns error as true
   useEffect(() => {
+    console.log("Signing out");
     if (error) {
       signOut();
     }
@@ -23,7 +27,7 @@ export default function AccountScreen({ navigation }) {
 
   function signOut() {
     AsyncStorage.removeItem("token");
-    navigation.navigate("SignIn");
+    dispatch(signOutAction());
   }
 
   return (
