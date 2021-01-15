6a6ca2f1176688ff465fe376faa03be542fec148
shengrong <shengrong_88@hotmail.com>
2021-01-15 12:03:09 +0800

    dark mode added

--- screens/AccountScreen.js
+++ screens/AccountScreen.js
@@ -1,13 +1,15 @@
 import React, { useState, useEffect } from "react";
-import { Button, StyleSheet, Text, View, ActivityIndicator } from "react-native";
+import { Button, StyleSheet, Text, View, ActivityIndicator, Switch, } from "react-native";
 import { commonStyles } from "../styles/commonStyles";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useUsername } from "../hooks/useAPI";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { signOutAction } from "../redux/ducks/blogAuth";
+import { toggleDarkMode } from "../redux/ducks/accountPrefs";
 
 export default function AccountScreen({ navigation }) {
   const [username, loading, error, refresh] = useUsername();
+  const isDarkModeOn = useSelector((state) => state.prefs.darkMode);
   const dispatch = useDispatch();
 
   // signs out if the useUsername hook returns error as true
@@ -31,12 +33,47 @@ export default function AccountScreen({ navigation }) {
   }
 
   return (
-    <View style={commonStyles.container}>
-      <Text>Account Screen</Text>
-      {loading ? <ActivityIndicator /> : <Text>{username}</Text>}
+    <View style={[
+      commonStyles.container,
+      isDarkModeOn && { backgroundColor: "black" },
+      ]}
+      >
+      <Text style={[styles.titleText, isDarkModeOn && { color: "white" }]}>
+        Account Screen
+      </Text>
+      {loading ? (
+        <ActivityIndicator />
+      ) : (
+        <Text style={styles.usernameText}>{username}</Text>
+      )}
+      <View style={styles.horizontalBlock}>
+      <Text style={isDarkModeOn && { color: "white" }}>Dark mode</Text>
+        <Switch
+          value={isDarkModeOn}
+          onValueChange={() => dispatch(toggleDarkMode())}
+        />
+      </View>
+      <Text> {isDarkModeOn ? "DARK MODE ON" : "DARK MODE OFF"}</Text>
       <Button title="Sign out" onPress={signOut} />
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  titleText: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 24,
+  },
+  usernameText: {
+    fontSize: 18,
+    color: "grey",
+    marginBottom: 12,
+  },
+  horizontalBlock: {
+    alignItems: "center",
+    justifyContent: "space-between",
+    width: "40%",
+    flexDirection: "row",
+  },
+});
